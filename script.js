1d0d445ec096b4a9d197195f5eac51d20c3cc3df
JesselZapanta <jesselzapanta9@gmail.com>
2026-03-08 10:56:48 +0800

    update calc history display

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const history = document.getElementById("history");
 let firstNumber = null;
 let operator = null;
 let waitingSecond = false;
-let expression = "";
+let justCalculated = false;
 
-function appendNumber(num){
+function appendNumber(num) {
 
-    if(waitingSecond){
+    if (waitingSecond) {
         display.value = num;
         waitingSecond = false;
     } else {
@@ -16,47 +16,55 @@ function appendNumber(num){
     }
 }
 
-function setOperator(op){
+function setOperator(op) {
 
-    if(firstNumber === null){
-        firstNumber = parseFloat(display.value);
-        expression = display.value + " " + op + " ";
-    } else if(!waitingSecond){
+    const current = parseFloat(display.value);
 
-        const secondNumber = parseFloat(display.value);
-        firstNumber = operate(firstNumber, secondNumber, operator);
+    if (justCalculated) {
+        history.innerText = current + " " + op;
+        firstNumber = current;
+        operator = op;
+        waitingSecond = true;
+        justCalculated = false;
+        return;
+    }
 
-        expression += display.value + " " + op + " ";
-        display.value = firstNumber;
+    if (firstNumber === null) {
+        firstNumber = current;
+    } 
+    else if (!waitingSecond) {
+        const result = operate(firstNumber, current, operator);
+        display.value = result;
+        firstNumber = result;
     }
 
     operator = op;
     waitingSecond = true;
 
-    history.innerText = expression;
+    history.innerText = firstNumber + " " + op;
 }
 
-function calculate(){
+function calculate() {
 
-    if(operator === null) return;
+    if (operator === null) return;
 
     const secondNumber = parseFloat(display.value);
 
-    expression += display.value + " =";
+    history.innerText = firstNumber + " " + operator + " " + secondNumber + " =";
 
     const result = operate(firstNumber, secondNumber, operator);
 
-    history.innerText = expression;
     display.value = result;
 
     firstNumber = result;
     operator = null;
     waitingSecond = true;
+    justCalculated = true;
 }
 
-function operate(a,b,op){
+function operate(a, b, op) {
 
-    switch(op){
+    switch (op) {
         case "+": return a + b;
         case "-": return a - b;
         case "*": return a * b;
@@ -64,22 +72,23 @@ function operate(a,b,op){
     }
 }
 
-function clearDisplay(){
+function clearDisplay() {
     display.value = "";
     history.innerText = "";
     firstNumber = null;
     operator = null;
-    expression = "";
+    waitingSecond = false;
+    justCalculated = false;
 }
 
-function deleteLast(){
-    display.value = display.value.slice(0,-1);
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
 }
 
-function developer(){
+function developer() {
     alert("Developer Information\n\nName: Jessel Zapanta\nRole: Programmer\nCourse: MSIT1");
 }
 
-function about(){
+function about() {
     alert("Application Information\n\nWeb Calculator\nVersion 1.0\nStandard Web Calculator.");
 }
